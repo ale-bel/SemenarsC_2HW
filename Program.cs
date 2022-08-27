5623aed4aa4c07696a61e9ed540aa9554306ae93
Aleksandra Belogubova <a.s.belogubova@gmail.com>
2022-08-27 21:13:39 +0300

    First task

--- Program.cs
+++ Program.cs
@@ -1,23 +1,4 @@
-﻿// int CutNumber () {
-//     int num = new Random ().Next (100, 1000);
-//     Console.WriteLine ("Number is " +num);
-//     int dec = num/100;
-//     int ed = num % 10;
-//     int result = dec*10 + ed;
-//     return result;
-// }
-// int number = CutNumber ();
-// Console.WriteLine ("Cut numbre is " +number);
+﻿//Напишите программу, которая принимает на вход трёхзначное число 
+//и на выходе показывает вторую цифру этого числа.
 
-int Factorial (int num) {
-    int current = 1;
-    int fact = 1;
-    while (current<=num) {
-        fact *= current;
-        current++;
-        }
-    return fact;
-}
-
-int result = Factorial(5) * Factorial(3);
-Console.WriteLine("Your result is " +result);
+void ThreeDigit (int num)
