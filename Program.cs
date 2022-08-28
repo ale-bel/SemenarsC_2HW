aa694660780aae3286473366b2d1560bc35b35b2
Aleksandra Belogubova <a.s.belogubova@gmail.com>
2022-08-28 09:58:39 +0300

    All tasks

--- Program.cs
+++ Program.cs
@@ -1,6 +1,8 @@
 ﻿//Напишите программу, которая принимает на вход трёхзначное число 
 //и на выходе показывает вторую цифру этого числа.
 
+// Первый вариант решения через функцию
+
 // int ThreeDigit (int num ){
  
 //         int result = (num/100) % 10;
@@ -12,6 +14,9 @@
 
 // ThreeDigit (num);
 // Console.WriteLine ("Result: " + result);
+
+
+// Второй вариант решения через массив
 
 // int [] array = new int [3];
 // int N = array Length;
@@ -26,18 +31,50 @@
 //     if (array [index] == find) 
 //     {
 // Console.WriteLine (index);
-// break;
+// return 0;
 
 //     }
 
 //     index++;
 // }
 
+// Console.WriteLine("Element in second index : " + find);
 
-// Console.WriteLine("Element in second index : " + numbers[1]);
+
+// Третий вариант решения через String
+// Console.Write("Enter the three digit number: ");
+// int threeDigitNumber = Convert.ToInt32(Console.ReadLine());
+// string stringNumber = Convert.ToString(threeDigitNumber);
+// Console.WriteLine("The second number -> "+stringNumber[1]);
 
 
-Console.Write("Enter the three digit number: ");
-int threeDigitNumber = Convert.ToInt32(Console.ReadLine());
-string stringNumber = Convert.ToString(threeDigitNumber);
-Console.WriteLine("The second number -> "+stringNumber[1]);
+// Напишите программу, которая выводит третью цифру заданного числа
+// или сообщает, что третьей цифры нет.
+
+// Console.Write("Enter number: ");
+// int Number = Convert.ToInt32(Console.ReadLine());
+// string NumberText = Convert.ToString(Number);
+// if (NumberText.Length > 2){
+//   Console.WriteLine("the third digit -> " + NumberText[2]);
+// }
+// else {
+//   Console.WriteLine("-> no the third digit");
+// }
+
+//Напишите программу, которая принимает на вход цифру, обозначающую день недели,
+//и проверяет, является ли этот день выходным.
+
+Console.Write("Номер дня недели: ");
+int NumDay = Convert.ToInt32(Console.ReadLine());
+
+void Checking (int NumDayber) {
+  if (NumDay == 6 || NumDay == 7) {
+  Console.WriteLine("этот день выходной -> да");
+  }
+  else if (NumDay < 1 || NumDay > 7) {
+    Console.WriteLine("ошибка, нет такого дня");
+  }
+  else Console.WriteLine("этот день выходной -> нет");
+}
+
+Checking(NumDay);
